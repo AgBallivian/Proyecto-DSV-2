d6811694a18f1f60abc9c5fe46950c8a58302085
AgBallivian <agballivian@miuandes.cl>
2024-06-19 04:13:38 -0400

    SLA Refractor

--- flask-server/algoritmo.py
+++ flask-server/algoritmo.py
@@ -34,6 +34,10 @@ class form_solver():
             self.adquirentes_data = formulario['adquirentes']
         except:
             self.adquirentes_data = []
+            
+    def _ejecutar_query(self, cursor, query):
+        cursor.execute(query)
+        return cursor.fetchall()
 
     def obtener_numer_de_atencion(self):
         connect = self.connection()
@@ -162,13 +166,9 @@ class form_solver():
         try:
             with connect.cursor() as cursor:
                 com_man_pred = self._construir_com_man_pred()
-                ano_final = self.obtener_ano_inscripcion() - 1
-                query_multipropietarios = QUERY_UPDATE_MULTIPROPIETARIO_SQL.format(
-                    ano_final=ano_final,
-                    last_initial_year=last_initial_year_in,
-                    com_man_pred=com_man_pred
-                )
-                print("Number of rows modified: ", cursor.execute(query_multipropietarios))
+                ano_final = self._obtener_ano_final()
+                query_multipropietarios = self._construir_query_actualizar_multipropietarios(ano_final, last_initial_year_in, com_man_pred)
+                self._ejecutar_query_actualizar_multipropietarios(cursor, query_multipropietarios)
                 connect.commit()
                 return True
         except Exception as e:
@@ -178,17 +178,28 @@ class form_solver():
         finally:
             connect.close()
 
+    def _obtener_ano_final(self):
+        return self.obtener_ano_inscripcion() - 1
+
+    def _construir_query_actualizar_multipropietarios(self, ano_final, last_initial_year, com_man_pred):
+        return QUERY_UPDATE_MULTIPROPIETARIO_SQL.format(
+            ano_final=ano_final,
+            last_initial_year=last_initial_year,
+            com_man_pred=com_man_pred
+        )
+
+    def _ejecutar_query_actualizar_multipropietarios(self, cursor, query):
+        print("Number of rows modified: ", cursor.execute(query))
+
+
+#revisar SUS METODOS ---
     def delete_multipropietarios_antiguos(self, last_initial_year):
-        print("SOY UN FURROOOOOOOOOOOOOOOOOOOO "+ str(last_initial_year))
         connect = self.connection()
-        com_man_pred = self._construir_com_man_pred()
         try:
             with connect.cursor() as cursor:
-                delete_vigencia_final_query = QUERY_DELETE_MULTIPROPIETARIOS.format(
-                    last_initial_year=str(last_initial_year),
-                    com_man_pred=com_man_pred
-                )
-                print("Number of rows modified: ", cursor.execute(delete_vigencia_final_query))
+                com_man_pred = self._construir_com_man_pred()
+                delete_vigencia_final_query = self._construir_query_delete_multipropietarios(last_initial_year, com_man_pred)
+                self._ejecutar_query(cursor, delete_vigencia_final_query)
                 connect.commit()
                 return True
         except Exception as e:
@@ -197,47 +208,36 @@ class form_solver():
             return jsonify({"error": str(e)}), INTERNAL_SERVER_ERROR
         finally:
             connect.close()
+
+    def _construir_query_delete_multipropietarios(self, last_initial_year, com_man_pred):
+        return QUERY_DELETE_MULTIPROPIETARIOS.format(
+            last_initial_year=str(last_initial_year),
+            com_man_pred=com_man_pred
+        )
+
 
     def obtener_multipropietarios(self):
-        com_man_pred = self._construir_com_man_pred()
         connect = self.connection()
         try:
             with connect.cursor() as cursor:
-                multipropietario_sql = QUERY_OBTENER_MULTIPROPIETARIOS_SQL.format(com_man_pred=com_man_pred)
-                cursor.execute(multipropietario_sql)
-                multipropietarios = cursor.fetchall()
-                return multipropietarios[0]['COUNT(*)']
+                com_man_pred = self._construir_com_man_pred()
+                multipropietario_sql = self._construir_query_obtener_multipropietarios(com_man_pred)
+                multipropietarios = self._ejecutar_query(cursor, multipropietario_sql)
+                return self._obtener_count_multipropietarios(multipropietarios)
         except Exception as e:
             connect.rollback()
             print("error: ", e)
             return jsonify({"error": str(e)}), INTERNAL_SERVER_ERROR
         finally:
             connect.close()
+    
+    def _construir_query_obtener_multipropietarios(self, com_man_pred):
+        return QUERY_OBTENER_MULTIPROPIETARIOS_SQL.format(com_man_pred=com_man_pred)
 
-#revisar
+    def _obtener_count_multipropietarios(self, multipropietarios):
+        return multipropietarios[0]['COUNT(*)']
+#End revisar SUS metodos ---
 
-    #def _obtener_suma_porc_Dercho_enjantes(self, Run_Rut_enajenantes):
-        print(Run_Rut_enajenantes)
-        com_man_pred = self._construir_com_man_pred()
-        runrut_enajenantes_formateado = formatear_runrut_enajenantes(Run_Rut_enajenantes)
-        print("HOLAAAAAAAAAAAAAAAAAAAAAAAAAA"+runrut_enajenantes_formateado)
-        connect = self.connection()
-        try:
-            with connect.cursor() as cursor:
-                multipropietario_sql = QUERY_OBTENER_SUMA_PORC_DERECHO_SQL.format(
-                com_man_pred=com_man_pred,
-                run_rut_enajenantes=runrut_enajenantes_formateado
-            )
-                print("multipropietario_sql :" + multipropietario_sql)
-                cursor.execute(multipropietario_sql)
-                multipropietarios = cursor.fetchall()
-                return int(multipropietarios[0]['sum'])
-        except Exception as e:
-            connect.rollback()
-            print("error: ", e)
-            return jsonify({"error": str(e)}), INTERNAL_SERVER_ERROR
-        finally:
-            connect.close() 
 
     def _construir_com_man_pred(self):
         return f"{self.comuna}-{self.manzana}-{self.predio}"
@@ -246,19 +246,8 @@ class form_solver():
         connect = self.connection()
         try:
             with connect.cursor() as cursor:
-                cursor.execute(QUERY_INSERTAR_ENAJENANTES_MULTIPROPIETARIO_SQL, (
-                    id_multipropietario,
-                    com_man_pred,
-                    enajenante['RUNRUT'],
-                    enajenante['porcDerecho'],
-                    self.fojas,
-                    int(self.obtener_ano_inscripcion()),
-                    self.numero_inscripcion,
-                    self.fecha_inscripcion,
-                    int(self.obtener_ano_inscripcion()),
-                    None,
-                    "Enajenante"
-                ))
+                query = self._construir_query_insertar_enajenantes(id_multipropietario, com_man_pred, enajenante)
+                self._ejecutar_query(cursor, query)
                 connect.commit()
         except Exception as e:
             connect.rollback()
@@ -266,24 +255,28 @@ class form_solver():
             return jsonify({"error": str(e)}), INTERNAL_SERVER_ERROR
         finally:
             connect.close()
+
+    def _construir_query_insertar_enajenantes(self, id_multipropietario, com_man_pred, enajenante):
+        return QUERY_INSERTAR_ENAJENANTES_MULTIPROPIETARIO_SQL, (
+            id_multipropietario,
+            com_man_pred,
+            enajenante['RUNRUT'],
+            enajenante['porcDerecho'],
+            self.fojas,
+            int(self.obtener_ano_inscripcion()),
+            self.numero_inscripcion,
+            self.fecha_inscripcion,
+            int(self.obtener_ano_inscripcion()),
+            None,
+            "Enajenante"
+        )
 
     def _insert_adquirientes_to_multipropietarios(self, id_multipropietario, com_man_pred, adquirente):
         connect = self.connection()
         try:
             with connect.cursor() as cursor:
-                cursor.execute(QUERY_INSERTAR_ADQUIRENTES_MULTIPROPIETARIO_SQL, (
-                    id_multipropietario,
-                    com_man_pred,
-                    adquirente['RUNRUT'],
-                    adquirente['porcDerecho'],
-                    self.fojas,
-                    int(self.obtener_ano_inscripcion()),
-                    self.numero_inscripcion,
-                    self.fecha_inscripcion,
-                    int(self.obtener_ano_inscripcion()),
-                    None,
-                    "Aquirente"
-                ))
+                query = self._construir_query_insertar_adquirientes(id_multipropietario, com_man_pred, adquirente)
+                self._ejecutar_query(cursor, query)
                 connect.commit()
         except Exception as e:
             connect.rollback()
@@ -292,15 +285,29 @@ class form_solver():
         finally:
             connect.close()
 
+    def _construir_query_insertar_adquirientes(self, id_multipropietario, com_man_pred, adquirente):
+        return QUERY_INSERTAR_ADQUIRENTES_MULTIPROPIETARIO_SQL, (
+            id_multipropietario,
+            com_man_pred,
+            adquirente['RUNRUT'],
+            adquirente['porcDerecho'],
+            self.fojas,
+            int(self.obtener_ano_inscripcion()),
+            self.numero_inscripcion,
+            self.fecha_inscripcion,
+            int(self.obtener_ano_inscripcion()),
+            None,
+            "Aquirente"
+        )
+
     def _obtener_ultimo_ano_inicial(self):
-        com_man_pred = self._construir_com_man_pred()
         connect = self.connection()
         try:
             with connect.cursor() as cursor:
-                cursor.execute(QUERY_OBTENER_ULT_ANO_INIT.format(com_man_pred=com_man_pred)
-                )
-                last_initial_year_query = cursor.fetchall()
-                return last_initial_year_query[0]['Ano']
+                com_man_pred = self._construir_com_man_pred()
+                query = self._construir_query_obtener_ultimo_ano_inicial(com_man_pred)
+                last_initial_year_query = self._ejecutar_query(cursor, query)
+                return self._obtener_ano_desde_query(last_initial_year_query)
         except Exception as e:
             connect.rollback()
             print("error: ", e)
@@ -308,20 +315,22 @@ class form_solver():
         finally:
             connect.close()
 
-    def agregar_nuevo_formulario(self):
+    def _construir_query_obtener_ultimo_ano_inicial(self, com_man_pred):
+        return QUERY_OBTENER_ULT_ANO_INIT.format(com_man_pred=com_man_pred)
 
+    def _obtener_ano_desde_query(self, query_result):
+        return query_result[0]['Ano']
+
+
+    def agregar_nuevo_formulario(self):
         numero_de_atencion = self.add_formulario()
-        self.add_enajenante(numero_de_atencion)
-        self.add_adquirente(numero_de_atencion)
-        self.add_multipropietario()
+        self.add_all(numero_de_atencion)
 
     def actualizar_vigencia(self, last_initial_year):
         self._actualizar_multipropietarios_por_vigencia(last_initial_year)
 
         numero_de_atencion = self.add_formulario()
-        self.add_enajenante(numero_de_atencion)
-        self.add_adquirente(numero_de_atencion)
-        self.add_multipropietario() 
+        self.add_all(numero_de_atencion)
         return True
 
     def procesar_escenario_3(self, last_initial_year):
@@ -330,52 +339,75 @@ class form_solver():
     def eliminar_antiguos_y_reemplazar(self, last_initial_year):
         self.delete_multipropietarios_antiguos(last_initial_year)
         numero_de_atencion = self.add_formulario()
+        self.add_all(numero_de_atencion)
+
+    def add_all(self, numero_de_atencion):
         self.add_enajenante(numero_de_atencion)
         self.add_adquirente(numero_de_atencion)
-        self.add_multipropietario() 
+        self.add_multipropietario()
 
     def determinar_y_procesar_escenario(self):
-        if(self.cne == COMPRAVENTA):
-            pass#TODO
+        if self.cne == COMPRAVENTA:
+            self.procesar_escenario_compraventa()
+        elif self.cne == REGULARIZACION_DE_PATRIMONIO:
+            self.procesar_escenario_regularizacion_patrimonio()
 
-        if(self.cne == REGULARIZACION_DE_PATRIMONIO):
-            count_multipropietario = self.obtener_multipropietarios()
-            if(count_multipropietario == 0):#esta malo arreglar
-                print("Escenario 1")
-                self.agregar_nuevo_formulario()
+    def procesar_escenario_compraventa(self):
+        # TODO: Implementar lógica para el escenario de compraventa
+        pass
+
+    def procesar_escenario_regularizacion_patrimonio(self):
+        count_multipropietario = self.obtener_multipropietarios()
+        if count_multipropietario == 0:
+            self.procesar_escenario_1()
+        else:
+            last_initial_year = self._obtener_ultimo_ano_inicial()
+            if last_initial_year < self.obtener_ano_inscripcion():
+                self.procesar_escenario_2(last_initial_year)
+            elif last_initial_year > self.obtener_ano_inscripcion():
+                self.procesar_escenario_3(last_initial_year)
             else:
-                last_initial_year = self._obtener_ultimo_ano_inicial()
-                if(last_initial_year < self.obtener_ano_inscripcion()):
-                    print("Escenario 2")
-                    self.actualizar_vigencia(last_initial_year)
-                if(last_initial_year > self.obtener_ano_inscripcion()):
-                    print("Es menor pero este escenario no esta listo")
-                    self.procesar_escenario_3(last_initial_year)
-                    #TODO
-                if(last_initial_year == self.obtener_ano_inscripcion()):
-                    print("Escenario 4")
-                    self.eliminar_antiguos_y_reemplazar(last_initial_year)
+                self.procesar_escenario_4(last_initial_year)
+
+    def procesar_escenario_1(self):
+        print("Escenario 1")
+        self.agregar_nuevo_formulario()
+
+    def procesar_escenario_2(self, last_initial_year):
+        print("Escenario 2")
+        self.actualizar_vigencia(last_initial_year)
+
+    def procesar_escenario_3(self, last_initial_year):
+        print("Es menor pero este escenario no esta listo")
+        # TODO: Implementar lógica para el escenario 3
+        pass
+
+    def procesar_escenario_4(self, last_initial_year):
+        print("Escenario 4")
+        self.eliminar_antiguos_y_reemplazar(last_initial_year)
 
     def ajustar_porcentajes_adquirentes(self):
-        sum_porc_Derecho_adquirente = 0
-        for adquirente in self.adquirentes_data:
-            porc_Derecho = int(adquirente.get('porcDerecho'))
-            if porc_Derecho is not None:
-                sum_porc_Derecho_adquirente += porc_Derecho
+        sum_porc_derecho_adquirente = self._calcular_suma_porc_derecho_adquirente()
+        print("Suma de porcentajes de derecho de adquirentes: " + str(sum_porc_derecho_adquirente))
 
-        print("q pijas " + str(sum_porc_Derecho_adquirente))
-        if sum_porc_Derecho_adquirente == 100:
-            Run_Rut_enajenantes = [enajenante['RUNRUT'] for enajenante in self.enajenantes_data]
-            if(Run_Rut_enajenantes != []):
-                sum_porc_Derecho_enajenante = 0
-                for enajenante in self.enajenantes_data:
-                    porc_Derecho = int(enajenante.get('porcDerecho'))
-                    if porc_Derecho is not None:
-                        sum_porc_Derecho_enajenante += porc_Derecho
-                for adquirente in self.adquirentes_data:
-                    porc_Derecho = int(adquirente.get('porcDerecho'))
-                    if porc_Derecho is not None:
-                        print()
-                        porc_Derecho *= sum_porc_Derecho_enajenante / 100
-                        adquirente['porcDerecho'] = str(porc_Derecho)
+        if sum_porc_derecho_adquirente == 100:
+            run_rut_enajenantes = self._obtener_run_rut_enajenantes()
+            if run_rut_enajenantes:
+                sum_porc_derecho_enajenante = self._calcular_suma_porc_derecho_enajenante()
+                self._ajustar_porc_derecho_adquirentes(sum_porc_derecho_enajenante)
                 print(self.adquirentes_data)
+
+    def _calcular_suma_porc_derecho_adquirente(self):
+        return sum(int(adquirente.get('porcDerecho', 0)) for adquirente in self.adquirentes_data)
+
+    def _obtener_run_rut_enajenantes(self):
+        return [enajenante['RUNRUT'] for enajenante in self.enajenantes_data]
+
+    def _calcular_suma_porc_derecho_enajenante(self):
+        return sum(int(enajenante.get('porcDerecho', 0)) for enajenante in self.enajenantes_data)
+
+    def _ajustar_porc_derecho_adquirentes(self, sum_porc_derecho_enajenante):
+        for adquirente in self.adquirentes_data:
+            porc_derecho = int(adquirente.get('porcDerecho', 0))
+            if porc_derecho != 0:
+                adquirente['porcDerecho'] = str(porc_derecho * sum_porc_derecho_enajenante / 100)
