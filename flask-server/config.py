c12fbcba621187726ae2a28bd2709999b381f243
lvdelrio <lvdelrio@miuandes.cl>
2024-06-24 23:50:10 -0400

    descomentando las claves

--- flask-server/config.py
+++ flask-server/config.py
@@ -1,8 +1,6 @@
 class Config:
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'admin'
-    # MYSQL_HOST = 'db'
-    # MYSQL_USER = 'root'
+    MYSQL_HOST = 'db'
+    MYSQL_USER = 'root'
     MYSQL_PASSWORD = 'admin'
     MYSQL_DB = 'proyectodsv'
     CORS_HEADERS = 'Content-Type'
